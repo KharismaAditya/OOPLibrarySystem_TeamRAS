48445251d438690ff602e00387cb4f68082d548c
KharismaAditya <yenderruaditya@gmail.com>
2025-05-22 15:53:49 +0700

    Here We Land!

--- src/ui/LoginGUI.java
+++ src/ui/LoginGUI.java
@@ -1,4 +1,11 @@
 package ui;
 
-public class LoginGUI {
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class LoginGUI extends Application {
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+
+    }
 }
